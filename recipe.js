9fff5ef17c79ec78d247329b498ec81a05975a14
Sam Rogers <samrogers15@gmail.com>
2020-12-22 15:03:19 -0800

    committing changes to source code

--- recipe.js
+++ recipe.js
@@ -12,23 +12,29 @@ $('.recipeBtn').on('click', generateRecipe);
 function generateRecipe() {
     var ingredients = 'onion,garlic,pepper';
     // new variable should be the string from the user input
-    var queryURL = "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=" + ingredients;
+    var recipeQueryURL = "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=" + ingredients;
     // console.log(ingredientQuery);
-    console.log(queryURL);
+    console.log(recipeQueryURL);
 
     $.ajax({
-        url: queryURL,
+        url: recipeQueryURL,
         method: "GET"
       }).then(function(response) {
         var responseParse = JSON.parse(response)
         responseParse.results.forEach(function(result){
             console.log(result.title);
             console.log(result.ingredients);
+            console.log(result.href);
         })
     });
+
+    // Append everything to the page
+    // Put the recipe in local storage - or maybe put the ingredients you've searched for in local storage? That way it could pull back and tell you to be inspirational and search for different ingredients
+
+
 };
 
-// generateRecipe();
+
 
 
 
