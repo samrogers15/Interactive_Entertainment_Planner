2e61a46a90522f8b773f40f5355d1840ca585b96
melissa-stott <1princessmelissa@gmail.com>
2020-12-28 20:04:40 -0800

    working on getting movie url to show up on screen

--- movie.js
+++ movie.js
@@ -7,19 +7,17 @@ $.ajax({
     method: "GET"
 }).then(function(responseMovie){
     console.log(responseMovie);
-    // const unixTimeCurrent = responseCurrent.dt;
-    // var weatherIcon = responseCurrent.weather[0].icon;
-    // var todaysIcon = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+    
 
 var movieSuggestion = $("<p></p>");
 movieSuggestion.text(responseMovie.Similar.Results[0].Name);
 var movieTrailerClip = responseMovie.Similar.Results[0].yUrl;
 var movieTrailer = $("<a href=responseMovie.Similar.Results[0].yUrl></a>");
-movieTrailer.attr(responseMovie.Similar.Results[0].wUrl);
+movieTrailer.attr(responseMovie.Similar.Results[0].yUrl);
 console.log(movieTrailer);
 console.log(responseMovie.Similar.Results[0].yUrl);
 
 // $('body').append('<div class = movieTitle></div>');
 $(movieResultsContainer).append(movieSuggestion);
-$(movieResultsContainer).append(movieTrailer);
+// $(movieResultsContainer).append(movieTrailer);
 })
