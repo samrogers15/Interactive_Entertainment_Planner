cf8e7d9a4a09f296a0f6fa421b6db208defd2bb9
melissa-stott <1princessmelissa@gmail.com>
2020-12-30 21:07:00 -0800

    added on click event to get user input, and display movie option on the screen

--- movie.js
+++ movie.js
@@ -1,23 +1,30 @@
-var userChoice = 'mermaids';
-var similarMovie = "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=" + userChoice + "&verbose=1&k=395755-Interact-E48M1JK8";
+console.log("https://tastedive.com/api/similar?q=what+dreams+may+come&verbose=1&k=395755-Interact-E48M1JK8");
 
-console.log(similarMovie);
+
+$(movieBtn).on('click', function(evt){
+    var userChoice = $(movieInput).val();
+    var similarMovie = "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=" + userChoice + "&verbose=1&k=395755-Interact-E48M1JK8";
+    console.log(similarMovie);
+
+
 $.ajax({
     url: similarMovie,
     method: "GET"
 }).then(function(responseMovie){
     console.log(responseMovie);
-    
+    $(movieResultsContainer).text(JSON.stringify(movieSuggestion))
 
+
+   
 var movieSuggestion = $("<p></p>");
 movieSuggestion.text(responseMovie.Similar.Results[0].Name);
 var movieTrailerClip = responseMovie.Similar.Results[0].yUrl;
-var movieTrailer = $("<a href=responseMovie.Similar.Results[0].yUrl></a>");
-movieTrailer.attr(responseMovie.Similar.Results[0].yUrl);
+var movieTrailer = $("<a></a>").attr('href', movieTrailerClip);
 console.log(movieTrailer);
 console.log(responseMovie.Similar.Results[0].yUrl);
 
 // $('body').append('<div class = movieTitle></div>');
 $(movieResultsContainer).append(movieSuggestion);
-// $(movieResultsContainer).append(movieTrailer);
+$(movieResultsContainer).append(movieTrailer);
 })
+});
