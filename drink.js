48a4da97041163b2cf03f326d41c6c34a35bda8e
Destiny Fleming <destiny.fleming7401@gmail.com>
2021-01-02 13:32:32 -0800

    finished the drink ajax and added ids

--- drink.js
+++ drink.js
@@ -1,67 +1,51 @@
 var drinkQueryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 
-$.ajax({
-    url: drinkQueryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(drinkQueryURL)
-    var drinkInfo = response.drinks[0];
-    console.log(drinkInfo)
-    drinkName = response.drinks[0].strDrink
-    console.log(drinkName)
-    drinkPic = response.drinks[0].strDrinkThumb
-    console.log(drinkPic)
 
-    //console.log ingredients
-    var drinkInfo = response.drinks[0];
-    console.log(drinkInfo)
+//create an onclicik event 
+$( "#drink-button" ).click(function() {
 
-    for (var i = 1; i <= 15; i++) {
-
-        var measure = eval("drinkInfo.strMeasure" + i);
-        var ingredient = eval("drinkInfo.strIngredient" + i);
-        console.log(measure + " " + ingredient);
-
-        if (!measure)
-            measure = "";
-        if (ingredient) {
-            //appending it to html
-            console.log("yes");
+    //add the ajax
+    $.ajax({
+        url: drinkQueryURL,
+        method: "GET"
+    }).then(function(response){
+    
+        console.log(drinkQueryURL)
+    
+        //console.log ingredients
+        var drinkInfo = response.drinks[0];
+        console.log(drinkInfo)
+    
+        //append drink name 
+        drinkName = response.drinks[0].strDrink
+        console.log(drinkName)
+        $("#drink-container-results").append("Drink Name: " + drinkName + "<br>");
+    
+        //and picture
+        drinkPic = response.drinks[0].strDrinkThumb
+        console.log(drinkPic)
+        var image = $("<img>").attr("src", drinkPic);
+        $("#drink-container-results").append(image);
+    
+        $("#drink-container-results").append("<br>" + "Ingredients:" + "<br>");
+    
+       // loop through and append ingredients plus measurments 
+        for (var i = 1; i <= 15; i++) {
+    
+            var measure = eval("drinkInfo.strMeasure" + i);
+            var ingredient = eval("drinkInfo.strIngredient" + i);
+            console.log(measure + " " + ingredient);
+    
+            if (measure === null)
+                console.log("null"); else 
+                $("#drink-container-results").append( measure + " " + ingredient + "<br>");
         }
-    }
-    /*console.log(response.drinks[0].strIngredient1)
-    console.log(response.drinks[0].strIngredient2)
-    console.log(response.drinks[0].strIngredient3)
-    console.log(response.drinks[0].strIngredient4)
-    console.log(response.drinks[0].strIngredient5)
-    console.log(response.drinks[0].strIngredient6)
-    console.log(response.drinks[0].strIngredient7)
-    console.log(response.drinks[0].strIngredient8)
-    console.log(response.drinks[0].strIngredient9)
-    console.log(response.drinks[0].strIngredient10)
-    console.log(response.drinks[0].strIngredient11)
-    console.log(response.drinks[0].strIngredient12)
-    console.log(response.drinks[0].strIngredient13)
-    console.log(response.drinks[0].strIngredient14)
-    console.log(response.drinks[0].strIngredient15)
-    //instructions
-    console.log(response.drinks[0].strInstructions)
-    //console.log measuring sizes
-    console.log(response.drinks[0].strMeasure1)
-    console.log(response.drinks[0].strMeasure2)
-    console.log(response.drinks[0].strMeasure3)
-    console.log(response.drinks[0].strMeasure4)
-    console.log(response.drinks[0].strMeasure5)
-    console.log(response.drinks[0].strMeasure6)
-    console.log(response.drinks[0].strMeasure7)
-    console.log(response.drinks[0].strMeasure8)
-    console.log(response.drinks[0].strMeasure9)
-    console.log(response.drinks[0].strMeasure10)
-    console.log(response.drinks[0].strMeasure11)
-    console.log(response.drinks[0].strMeasure12)
-    console.log(response.drinks[0].strMeasure13)
-    console.log(response.drinks[0].strMeasure14)
-    console.log(response.drinks[0].strMeasure15)*/
+    
+        //append instructions
+        var drinkInst = response.drinks[0].strInstructions;
+        console.log(drinkInst)
+        $("#drink-container-results").append("Instructions: " + drinkInst + "<br>");
 
-    $("#drink-container-results").text("drink name: " + drinkName);
-})
+    })
+    
+});
