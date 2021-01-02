35429aa6ea8dea035c54ec3875ffbb6aca8bbc18
melissa-stott <1princessmelissa@gmail.com>
2021-01-02 13:19:38 -0800

    added code to open link in a separate page

--- movie.js
+++ movie.js
@@ -1,11 +1,7 @@
-console.log("https://tastedive.com/api/similar?q=what+dreams+may+come&verbose=1&k=395755-Interact-E48M1JK8");
-
-
 $(movieBtn).on('click', function(evt){
     var userChoice = $(movieInput).val();
-    var similarMovie = "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=" + userChoice + "&verbose=1&k=395755-Interact-E48M1JK8";
-    console.log(similarMovie);
-
+    var similarMovie = "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=" + userChoice + "&limit=4&verbose=1&k=395755-Interact-E48M1JK8";
+   
 
 $.ajax({
     url: similarMovie,
@@ -13,18 +9,27 @@ $.ajax({
 }).then(function(responseMovie){
     console.log(responseMovie);
     $(movieResultsContainer).text(JSON.stringify(movieSuggestion))
-
-
-   
+    
+    var movieArray = responseMovie.Similar.Results
+    for (let i = 0; i < movieArray.length; i++) {
+        // const element = responseMovie[i];
+        console.log(movieArray[i].Name);
+        console.log(movieArray[i].yUrl);
+        
+       
     var movieSuggestion = $("<p></p>");
-    movieSuggestion.text(responseMovie.Similar.Results[0].Name);
-    var movieTrailerClip = responseMovie.Similar.Results[0].yUrl;
-    var movieTrailer = $("<a></a>").attr('href', movieTrailerClip).text(JSON.stringify(responseMovie.Similar.Results[0].Name));
+    var movieSuggestion = movieArray[i].Name;
+    var movieTrailerClip = movieArray[i].yUrl;
+    var movieTrailer = $("<a></a>").attr(
+        {href: movieTrailerClip,
+            target: "blank"
+    }).text(JSON.stringify(movieSuggestion));
     console.log(movieTrailer);
     console.log(responseMovie.Similar.Results[0].yUrl);
 
     // $('body').append('<div class = movieTitle></div>');
-    $(movieResultsContainer).append(movieSuggestion);
+    // $(movieResultsContainer).append(movieSuggestion);
     $(movieResultsContainer).append(movieTrailer);
+}
 })
 });
