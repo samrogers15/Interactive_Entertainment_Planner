b93ba9efc30b98c5da8a72063c064835e55d2846
Sam Rogers <samrogers15@gmail.com>
2021-01-03 21:09:15 -0800

    updating source code

--- recipe.js
+++ recipe.js
@@ -42,7 +42,7 @@ if (previousIngredients != null) {
 function previouslySearchedIngredients() {
     var previousIngredientsDiv = $('<div>').attr('class', 'previousIngredients').text(('You\'ve already searched for these ingredients: ' + previousIngredients + ' '));
     console.log(previousIngredients);
-    $('body').append(previousIngredientsDiv);
+    $(recipeResultsContainer).append(previousIngredientsDiv);
 }
 
 $('#previousIngredientsBtn').on('click', previouslySearchedIngredients);
